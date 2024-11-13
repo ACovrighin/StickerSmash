fa3085213a72f4cadfb7ec0e9c8a290e3d1c0404
ACovrighin <andrew44291@gmail.com>
2024-11-12 21:52:04 -0500

    First commit: What I started with

---
+++ app/+not-found.tsx
@@ -0,0 +1,34 @@
+import { Text, View, StyleSheet } from "react-native";
+import {Link, Stack} from 'expo-router';
+
+export default function NotFoundScreen() {
+  return (
+    <>
+    <Stack.Screen options = {{title: 'Oops! Not Found'}}/>
+    <View style={styles.container}>
+      <Link href = "/" style={styles.button}>
+      Go back to Home screen!
+      </Link>
+    </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create(
+  {
+    container: {
+      flex:1,
+      backgroundColor: '#23292e',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    text: {
+      color: '#fff'
+    },
+    button: {
+      fontSize:20,
+      textDecorationLine: 'underline',
+      color: '#fff'
+    },
+  }
+)
